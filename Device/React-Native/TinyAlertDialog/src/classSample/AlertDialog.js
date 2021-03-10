61be9b55134f258fb238c2060163e9c132b1cc2e
PCloud <pcloud63514@gmail.com>
2021-03-10 10:23:10 +0900

    push CloseEvent to children

--- Device/React-Native/TinyAlertDialog/src/classSample/AlertDialog.js
+++ Device/React-Native/TinyAlertDialog/src/classSample/AlertDialog.js
@@ -4,12 +4,14 @@ import { Modal, View, TouchableNativeFeedback } from 'react-native'
 export default props => {
     const {visible, close, children} = props
 
+    const wrapChildren = children != undefined ? React.cloneElement(children) : undefined
     return (
         <Modal
             visible={visible}
             transparent={true}
             animationType={'slide'}>
-            <View style={{flex:1,
+            <View 
+                style={{flex:1,
                     backgroundColor: "rgba(0,0,0,0.3)",
                     justifyContent:"center",
                     alignContent:"center",
@@ -19,7 +21,7 @@ export default props => {
 
                 </View>
             </TouchableNativeFeedback>
-                {children}   
+                {wrapChildren}
             </View>
             
         </Modal>
