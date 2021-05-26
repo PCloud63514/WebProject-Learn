42f5ee50eba9610b98e817a9227f156b87ccb2cb
PCloud63514 <PCloud63514@gmail.com>
2021-05-26 23:20:21 +0900

    fix

--- BackEnd/SpringBoot/TinySpringBoot 예제/tiny-spring-boot/src/main/java/com/pcloud/tinyspringboot/HomeController.java
+++ BackEnd/SpringBoot/TinySpringBoot 예제/tiny-spring-boot/src/main/java/com/pcloud/tinyspringboot/HomeController.java
@@ -3,15 +3,39 @@ package com.pcloud.tinyspringboot;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/home")
 @Controller
-public class HomeController {
+public class  HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/static")
     public String home(Model model, @RequestParam(required = false) String name) {
         System.out.println(name);
         model.addAttribute("data", name);
         return "home";
+    }
+
+    @GetMapping("/api")
+    @ResponseBody
+    public Home home(@RequestParam(required = false) String name) {
+        Home home = new Home();
+        home.setName(name);
+
+        return home;
+    }
+
+    static class Home {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
--- BackEnd/SpringBoot/TinySpringBoot 예제/tiny-spring-boot/src/main/java/com/pcloud/tinyspringboot/TinySpringBootApplication.java
+++ BackEnd/SpringBoot/TinySpringBoot 예제/tiny-spring-boot/src/main/java/com/pcloud/tinyspringboot/TinySpringBootApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TinySpringBootApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TinySpringBootApplication.class, args);
 	}
-
 }
