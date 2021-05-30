631dc56f89a2e1e260566e93a88d30596e26a0c3
PCloud63514 <PCloud63514@gmail.com>
2021-05-30 22:45:07 +0900

    설계 끝

--- BackEnd/SpringBoot/TinySpringBoot 예제/tiny-product-shop/src/main/java/com/pcloud/tinyproductshop/order/domain/Order.java
+++ BackEnd/SpringBoot/TinySpringBoot 예제/tiny-product-shop/src/main/java/com/pcloud/tinyproductshop/order/domain/Order.java
@@ -19,13 +19,31 @@ public class Order {
     @JoinColumn(name="member_id")
     private Member member;
 
-    @OneToMany(mappedBy="order")
+    @OneToMany(mappedBy="order", cascade=CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
-    @OneToOne(fetch=FetchType.LAZY)
+
+    // cascade -ALL 이 붙으므로, Order가 persist 되면 옵션이 붙은 객체도 같이 persist 된다.
+    @OneToOne(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
     @JoinColumn(name="delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    //==연관 관계 메서드//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
