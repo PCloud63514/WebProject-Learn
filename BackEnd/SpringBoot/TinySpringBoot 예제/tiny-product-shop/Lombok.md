d38f962fba29155976a6e5ae53a78de3424de961
PCloud63514 <PCloud63514@gmail.com>
2021-05-30 21:57:54 +0900

    Entity 1차 설계

---
+++ BackEnd/SpringBoot/TinySpringBoot 예제/tiny-product-shop/Lombok.md
@@ -0,0 +1,11 @@
+# Lombok
+
+
+
+
+
+
+
+### Setting
+
+Setting -> Buid, Execution, Deployment -> Compiler -> Annotation Processors 에서 Enable annotation processing 체크
---
+++ BackEnd/SpringBoot/TinySpringBoot 예제/tiny-product-shop/src/main/java/com/pcloud/tinyproductshop/TinyProductShopApplication.java
@@ -0,0 +1,13 @@
+package com.pcloud.tinyproductshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyProductShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyProductShopApplication.class, args);
+	}
+
+}
