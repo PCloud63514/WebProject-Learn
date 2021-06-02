8872f50d1905b17ce7725c7c69083d00862a8f14
PCloud63514 <PCloud63514@gmail.com>
2021-06-02 23:35:35 +0900

    order 추가 완료

---
+++ BackEnd/SpringBoot/TinySpringBoot 예제/tiny-product-shop/src/main/java/com/pcloud/tinyproductshop/order/controller/OrderForm.java
@@ -0,0 +1,14 @@
+package com.pcloud.tinyproductshop.order.controller;
+
+import com.pcloud.tinyproductshop.member.domain.Member;
+import com.pcloud.tinyproductshop.order.domain.OrderStatus;
+import com.pcloud.tinyproductshop.product.domain.Product;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Getter @Setter
+public class OrderForm {
+
+}
