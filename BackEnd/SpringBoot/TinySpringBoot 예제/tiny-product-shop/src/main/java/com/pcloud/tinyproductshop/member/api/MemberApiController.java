338c723690f371894d87ebc5b6f1b05d8ce61494
PCloud63514 <PCloud63514@gmail.com>
2021-06-04 00:41:18 +0900

    test db 코드 추가

--- BackEnd/SpringBoot/TinySpringBoot 예제/tiny-product-shop/src/main/java/com/pcloud/tinyproductshop/member/api/MemberApiController.java
+++ BackEnd/SpringBoot/TinySpringBoot 예제/tiny-product-shop/src/main/java/com/pcloud/tinyproductshop/member/api/MemberApiController.java
@@ -1,6 +1,5 @@
 package com.pcloud.tinyproductshop.member.api;
 
-import com.pcloud.tinyproductshop.member.controller.MemberForm;
 import com.pcloud.tinyproductshop.member.domain.Address;
 import com.pcloud.tinyproductshop.member.domain.Member;
 import com.pcloud.tinyproductshop.member.dto.MemberDto;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @RestController
