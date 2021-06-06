8731ef71f7c18b352f6f99e3501167770711790d
PCloud63514 <PCloud63514@gmail.com>
2021-06-06 19:15:07 +0900

    Order api v3 변경

--- BackEnd/SpringBoot/TinySpringBoot 예제/tiny-product-shop/src/main/java/com/pcloud/tinyproductshop/order/repository/OrderRepository.java
+++ BackEnd/SpringBoot/TinySpringBoot 예제/tiny-product-shop/src/main/java/com/pcloud/tinyproductshop/order/repository/OrderRepository.java
@@ -67,10 +67,24 @@ public class OrderRepository implements IOrderRepository {
     }
 
     public List<Order> findAllWithMemberDelivery() {
+        /*
+            원래 sql에서 distinct 는 조회한 데이터 row의 결과가 전부 동일해야 중복으로 취급되고 제거되었지만
+            jpa는 sql에 distinct 를 추가하고, 검색한 결과의 데이터에서 같은 Entity가 조회되면(id 기준)
+            애플리케이션에서 중복을 제외한다.
+            페이징이 불가능해진다.
+            .setFirstResult(0)  - offset
+            .setMaxResults(100) - limit
+            을 사용하면 applying in memory가 발생
+            이로 써 알게된 사실은 1대다 fetch 조회는 페이징을 해서는 안된다.
+            또한 1대다 fetch 조회는 1개만 사용할 수 있다.
+         */
+
         return em.createQuery(
-                "select o from Order o" +
+                "select distinct o from Order o" +
                         " join fetch o.member m" +
-                        " join fetch o.delivery d", Order.class
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.product p", Order.class
         ).getResultList();
     }
 }
