cb1ad0eb82f2e3afa945cd4fafab21f8d4a62b05
PCloud63514 <PCloud63514@gmail.com>
2021-06-07 22:34:58 +0900

    카카오 로그인 예제 시작

---
+++ BackEnd/SpringBoot/Kakao로그인 예제/tiny-kakao-login/src/main/java/com/pcloud/tinykakaologin/TinyKakaoLoginApplication.java
@@ -0,0 +1,13 @@
+package com.pcloud.tinykakaologin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyKakaoLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyKakaoLoginApplication.class, args);
+	}
+
+}
