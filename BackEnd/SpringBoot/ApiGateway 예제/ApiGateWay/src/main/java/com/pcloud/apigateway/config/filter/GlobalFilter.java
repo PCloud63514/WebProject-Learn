cc9d84fbd480499f263b57ce5af15997df5c7363
PCloud <pcloud63514@gmail.com>
2021-06-15 17:32:02 +0900

    slf4j

--- BackEnd/SpringBoot/ApiGateway 예제/ApiGateWay/src/main/java/com/pcloud/apigateway/config/filter/GlobalFilter.java
+++ BackEnd/SpringBoot/ApiGateway 예제/ApiGateWay/src/main/java/com/pcloud/apigateway/config/filter/GlobalFilter.java
@@ -1,16 +1,15 @@
 package com.pcloud.apigateway.config.filter;
 
 import lombok.Data;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
-    private static final Logger logger = LogManager.getLogger(GlobalFilter.class);
     public GlobalFilter() {
         super(Config.class);
     }
@@ -18,13 +17,13 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
     @Override
     public GatewayFilter apply(Config config) {
         return ((exchange, chain) -> {
-            logger.info("GlobalFilter baseMessage>>>>>>" + config.getBaseMessage());
+            log.info("GlobalFilter baseMessage>>>>>>" + config.getBaseMessage());
             if (config.isPreLogger()) {
-                logger.info("GlobalFilter Start>>>>>>" + exchange.getRequest());
+                log.info("GlobalFilter Start>>>>>>" + exchange.getRequest());
             }
             return chain.filter(exchange).then(Mono.fromRunnable(()->{
                 if (config.isPostLogger()) {
-                    logger.info("GlobalFilter End>>>>>>" + exchange.getResponse());
+                    log.info("GlobalFilter End>>>>>>" + exchange.getResponse());
                 }
             }));
         });
