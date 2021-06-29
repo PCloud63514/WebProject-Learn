f030c7ed12ca3dbc63c0aaeb782ffec43476d307
PCloud <pcloud63514@gmail.com>
2021-06-29 14:26:20 +0900

    Reflect를 이용한 DI

---
+++ Device/React-Native/Reflect 사용하기/ReactNativeReflect/src/components/Boo.ts
@@ -0,0 +1,8 @@
+import { Component } from "@decorator/index";
+
+@Component
+export default class Boo {
+  hello() {
+    console.log("Hello Boo")
+  }
+}
---
+++ Device/React-Native/Reflect 사용하기/ReactNativeReflect/src/components/Coo.ts
@@ -0,0 +1,8 @@
+import { Component } from "@decorator/index";
+
+@Component
+export default class Coo {
+  hello() {
+    console.log("Hello Coo")
+  }
+}
